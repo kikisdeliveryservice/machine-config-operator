0568a63b6341c3d95ce619e17524a1486828efd5
Kirsten G <kikis.github@gmail.com>
2020-01-21 14:28:56 -0800

    mcc: vendor ign3 types & run make go-deps

--- pkg/controller/common/helpers.go
+++ pkg/controller/common/helpers.go
@@ -4,17 +4,18 @@ import (
 	"io/ioutil"
 	"reflect"
 
-	igntypes "github.com/coreos/ignition/config/v2_2/types"
+	ign2types "github.com/coreos/ignition/config/v2_2/types"
 	validate "github.com/coreos/ignition/config/validate"
+	ign3types "github.com/coreos/ignition/v2/config/v3_0/types"
 	"github.com/golang/glog"
 	errors "github.com/pkg/errors"
 )
 
 // NewIgnConfig returns an empty ignition config with version set as latest version
-func NewIgnConfig() igntypes.Config {
+func NewIgnConfig() ign2types.Config {
 	return igntypes.Config{
-		Ignition: igntypes.Ignition{
-			Version: igntypes.MaxVersion.String(),
+		Ignition: ign2types.Ignition{
+			Version: ign2types.MaxVersion.String(),
 		},
 	}
 }
@@ -26,14 +27,30 @@ func WriteTerminationError(err error) {
 	glog.Fatal(msg)
 }
 
-// ValidateIgnition wraps the underlying Ignition validation, but explicitly supports
+// ValidateIgnition2 wraps the underlying Ignition validation, but explicitly supports
 // a completely empty Ignition config as valid.  This is because we
 // want to allow MachineConfig objects which just have e.g. KernelArguments
 // set, but no Ignition config.
 // Returns nil if the config is valid (per above) or an error containing a Report otherwise.
-func ValidateIgnition(cfg igntypes.Config) error {
+func ValidateIgnition2(cfg ign2types.Config) error {
 	// only validate if Ignition Config is not empty
-	if reflect.DeepEqual(igntypes.Config{}, cfg) {
+	if reflect.DeepEqual(ign2types.Config{}, cfg) {
+		return nil
+	}
+	if report := validate.ValidateWithoutSource(reflect.ValueOf(cfg)); report.IsFatal() {
+		return errors.Errorf("invalid Ignition config found: %v", report)
+	}
+	return nil
+}
+
+// ValidateIgnition3 wraps the underlying Ignition validation, but explicitly supports
+// a completely empty Ignition config as valid.  This is because we
+// want to allow MachineConfig objects which just have e.g. KernelArguments
+// set, but no Ignition config.
+// Returns nil if the config is valid (per above) or an error containing a Report otherwise.
+func ValidateIgnition3(cfg ign3types.Config) error {
+	// only validate if Ignition Config is not empty
+	if reflect.DeepEqual(ign3types.Config{}, cfg) {
 		return nil
 	}
 	if report := validate.ValidateWithoutSource(reflect.ValueOf(cfg)); report.IsFatal() {
